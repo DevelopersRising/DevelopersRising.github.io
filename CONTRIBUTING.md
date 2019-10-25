3ff7466f4e465b6d2bc2d068867d658f06f3dd17
Robert Axelsen <robert.axelsen@ebcont.com>
2019-10-25 17:25:14 +0200

    chore: discontinue website due to ngo being dissolved

--- CONTRIBUTING.md
+++
@@ -1,1 +0,0 @@
-https://github.com/DevelopersRising/DevelopersRising/blob/master/Contributing.md
--- public/index.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-  $(".parallax").parallax();
-  var $grid = $('.grid').masonry({
-    // options...
-  });
-});
